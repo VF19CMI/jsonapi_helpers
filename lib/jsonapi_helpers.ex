e19c0d0895f1f33ec05cee22a5600c8bf9dc0f81
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-02-16 13:13:22 +0000

    refactor

--- lib/jsonapi_helpers.ex
+++ lib/jsonapi_helpers.ex
@@ -1,58 +1,35 @@
 defmodule JsonapiHelpers do
   alias JsonapiHelpers.MapHelpers
 
-  def jsonapi_id(%{"data" => data}) do
-    data
-    |> MapHelpers.stringify_keys()
-    |> get_in(["id"])
+  def jsonapi_id(data) do
+    dig(data, ["data", "id"]) || dig(data, ["id"])
   end
 
-  def jsonapi_id(data), do: jsonapi_id(%{"data" => data})
-
-  def jsonapi_type(%{"data" => data}) do
-    data
-    |> MapHelpers.stringify_keys()
-    |> get_in(["type"])
+  def jsonapi_type(data) do
+    dig(data, ["data", "type"]) || dig(data, ["type"])
   end
 
-  def jsonapi_type(data), do: jsonapi_type(%{"data" => data})
-
-  def jsonapi_attributes(%{"data" => data}) do
-    data
-    |> MapHelpers.stringify_keys()
-    |> Map.get("attributes", %{})
-    |> Map.keys()
+  def jsonapi_attributes(data) do
+    Map.keys(dig(data, ["data", "attributes"]) || dig(data, ["attributes"]) || %{})
   end
 
-  def jsonapi_attributes(data), do: jsonapi_attributes(%{"data" => data})
-
-  def jsonapi_attribute_value(%{"data" => data}, attribute) do
-    data
-    |> MapHelpers.stringify_keys()
-    |> Map.get("attributes", %{})
+  def jsonapi_attribute_value(data, attribute) do
+    (dig(data, ["data", "attributes"]) || dig(data, ["attributes"]) || %{})
     |> Map.get(attribute)
   end
 
-  def jsonapi_attribute_value(data, attribute),
-    do: jsonapi_attribute_value(%{"data" => data}, attribute)
-
-  def jsonapi_relationships(%{"data" => data}) do
-    data
-    |> MapHelpers.stringify_keys()
-    |> Map.get("relationships", %{})
+  def jsonapi_relationships(data) do
+    (dig(data, ["data", "relationships"]) || dig(data, ["relationships"]) || %{})
     |> Map.keys()
   end
 
-  def jsonapi_relationships(data), do: jsonapi_relationships(%{"data" => data})
-
-  def jsonapi_relationship_data(%{"data" => data}, attribute) do
-    data
-    |> MapHelpers.stringify_keys()
-    |> Map.get("relationships", %{})
-    |> Map.get(attribute, %{})
-    |> Map.get("data")
+  def jsonapi_relationship_data(data, attribute) do
+    dig(data, ["data", "relationships", attribute, "data"]) || dig(data, ["relationships", attribute, "data"])
   end
 
-  def jsonapi_relationship_data(data, attribute),
-    do: jsonapi_relationship_data(%{"data" => data}, attribute)
+  defp dig(data, dig) do
+    data
+    |> MapHelpers.stringify_keys()
+    |> get_in(dig)
+  end
 end
