137a5a92f22d00ecdd8b185ee07e058608c1891c
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-02-16 13:03:12 +0000

    optionally dig in data

--- lib/jsonapi_helpers.ex
+++ lib/jsonapi_helpers.ex
@@ -1,48 +1,58 @@
 defmodule JsonapiHelpers do
   alias JsonapiHelpers.MapHelpers
 
-  def jsonapi_id(data) do
+  def jsonapi_id(%{"data" => data}) do
     data
     |> MapHelpers.stringify_keys()
-    |> get_in(["data", "id"])
+    |> get_in(["id"])
   end
 
-  def jsonapi_type(data) do
+  def jsonapi_id(data), do: jsonapi_id(%{"data" => data})
+
+  def jsonapi_type(%{"data" => data}) do
     data
     |> MapHelpers.stringify_keys()
-    |> get_in(["data", "type"])
+    |> get_in(["type"])
   end
 
-  def jsonapi_attributes(data) do
+  def jsonapi_type(data), do: jsonapi_type(%{"data" => data})
+
+  def jsonapi_attributes(%{"data" => data}) do
     data
     |> MapHelpers.stringify_keys()
-    |> Map.get("data", %{})
     |> Map.get("attributes", %{})
     |> Map.keys()
   end
 
-  def jsonapi_attribute_value(data, attribute) do
+  def jsonapi_attributes(data), do: jsonapi_attributes(%{"data" => data})
+
+  def jsonapi_attribute_value(%{"data" => data}, attribute) do
     data
     |> MapHelpers.stringify_keys()
-    |> Map.get("data", %{})
     |> Map.get("attributes", %{})
     |> Map.get(attribute)
   end
 
-  def jsonapi_relationships(data) do
+  def jsonapi_attribute_value(data, attribute),
+    do: jsonapi_attribute_value(%{"data" => data}, attribute)
+
+  def jsonapi_relationships(%{"data" => data}) do
     data
     |> MapHelpers.stringify_keys()
-    |> Map.get("data", %{})
     |> Map.get("relationships", %{})
     |> Map.keys()
   end
 
-  def jsonapi_relationship_data(data, attribute) do
+  def jsonapi_relationships(data), do: jsonapi_relationships(%{"data" => data})
+
+  def jsonapi_relationship_data(%{"data" => data}, attribute) do
     data
     |> MapHelpers.stringify_keys()
-    |> Map.get("data", %{})
     |> Map.get("relationships", %{})
     |> Map.get(attribute, %{})
     |> Map.get("data")
   end
+
+  def jsonapi_relationship_data(data, attribute),
+    do: jsonapi_relationship_data(%{"data" => data}, attribute)
 end
--- test/jsonapi_helpers_test.exs
+++ test/jsonapi_helpers_test.exs
@@ -3,22 +3,25 @@ defmodule JsonapiHelpersTest do
   import JsonapiHelpers
 
   setup do
-    [
-      params: %{
-        "data" => %{
-          "id" => "10",
-          "type" => "users",
-          "attributes" => %{
-            "email" => "jon@snow.com",
-            "name" => "Jon Snow",
-            "date" => ~N[2000-01-01 00:00:00]
-          },
-          "relationships" => %{
-            "comments" => %{"data" => [%{"id" => "10", "type" => "comments"}]},
-            "posts" => %{"data" => [%{"id" => "1", "type" => "posts"}]}
-          }
+    params = %{
+      "data" => %{
+        "id" => "10",
+        "type" => "users",
+        "attributes" => %{
+          "email" => "jon@snow.com",
+          "name" => "Jon Snow",
+          "date" => ~N[2000-01-01 00:00:00]
+        },
+        "relationships" => %{
+          "comments" => %{"data" => [%{"id" => "10", "type" => "comments"}]},
+          "posts" => %{"data" => [%{"id" => "1", "type" => "posts"}]}
         }
       }
+    }
+
+    [
+      params: params,
+      params_without_data: params["data"]
     ]
   end
 
@@ -47,5 +50,34 @@ defmodule JsonapiHelpersTest do
   test "jsonapi_relationship_data", %{params: params} do
     expected = [%{"id" => "10", "type" => "comments"}]
     assert jsonapi_relationship_data(params, "comments") == expected
+  end
+
+  describe "with missing data" do
+    test "jsonapi_id", %{params_without_data: params_without_data} do
+      assert jsonapi_id(params_without_data) == "10"
+    end
+
+    test "jsonapi_type", %{params_without_data: params_without_data} do
+      assert jsonapi_type(params_without_data) == "users"
+    end
+
+    test "jsonapi_attributes", %{params_without_data: params_without_data} do
+      assert jsonapi_attributes(params_without_data) == ~w(date email name)
+    end
+
+    test "jsonapi_attribute_value", %{params_without_data: params_without_data} do
+      assert jsonapi_attribute_value(params_without_data, "name") == "Jon Snow"
+      assert jsonapi_attribute_value(params_without_data, "email") == "jon@snow.com"
+      assert jsonapi_attribute_value(params_without_data, "date") == ~N[2000-01-01 00:00:00]
+    end
+
+    test "jsonapi_relationships", %{params_without_data: params_without_data} do
+      assert jsonapi_relationships(params_without_data) == ~w(comments posts)
+    end
+
+    test "jsonapi_relationship_data", %{params_without_data: params_without_data} do
+      expected = [%{"id" => "10", "type" => "comments"}]
+      assert jsonapi_relationship_data(params_without_data, "comments") == expected
+    end
   end
 end
